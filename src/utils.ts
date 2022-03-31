937c5c0c7d0d7a04e466cb18758addbf2523242c
fayeed <fayeed@live.com>
2022-03-31 12:05:12 +0530

    Moved all the utility functions to a seperate folder

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,13 @@
+import chalk from "chalk";
+import findProcess from "find-process";
+import glob from "glob";
+import { kill } from "process";
+
+export const logger = {
+  info: (...args: any[]) => !global.hideLogs && console.log(chalk.blue('[INFO]'),...args),
+  error: (...args: any[]) => !global.hideLogs && console.error(chalk.red('[ERROR]'), ...args)
+}
+
 export function cleanOutput(data: string) {
   const lines = data.split('\n')
   lines.splice(0, 1);
@@ -10,4 +20,23 @@ export function cleanOutput(data: string) {
   }
 
   throw Error('cannot be parsed');
+}
+
+export async function findAndKillProcess(processName: string) {
+  try {
+    const list = await findProcess('name', processName)
+
+    for (const p of list) {      
+      kill(p.pid, 'SIGKILL')
+    }
+  } catch (err) {
+    logger.error(`No process found by name: ${processName}`)
+  } 
+}
+
+export function getTestFiles() {
+  const testFiles = glob.sync('**/*.test.js').filter(e => !e.includes('node_modules'))
+  logger.info(testFiles)
+
+  return testFiles;
 }
